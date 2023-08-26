87364b05fb358983b03493af3fa8429e558a1b1e
Zachary Stence <zachstence@gmail.com>
2023-08-26 13:14:18 -0500

    chore: configure prettier and eslint

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
-	import { io, Socket } from 'socket.io-client';
-	import { onMount } from 'svelte';
+  import { io, Socket } from 'socket.io-client';
+  import { onMount } from 'svelte';
 
-	const WSS_URL = 'http://localhost:3000';
+  const WSS_URL = 'http://localhost:3000';
 
-	let socket: Socket;
+  let socket: Socket;
 
-	onMount(() => {
-		socket = io(WSS_URL);
-	});
+  onMount(() => {
+    socket = io(WSS_URL);
+  });
 
-	$: {
-		socket?.on('connect', () => {
-			console.log('connect');
-		});
-	}
+  $: {
+    socket?.on('connect', () => {
+      console.log('connect');
+    });
+  }
 </script>
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,23 +1,23 @@
-import express from 'express'
-import http from 'http'
-import cors from 'cors'
-import { Server } from 'socket.io'
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
+import { Server } from 'socket.io';
 
-const PORT = 3000
+const PORT = 3000;
 
 const app = express();
 app.use(cors());
 const httpServer = http.createServer(app);
 const wss = new Server(httpServer, {
-    cors: {
-        origin: '*',
-    }
+  cors: {
+    origin: '*',
+  },
 });
 
-wss.on('connection', (socket) => {
-    console.log(`connection ${socket.id}`)
-})
+wss.on('connection', socket => {
+  console.log(`connection ${socket.id}`);
+});
 
 httpServer.listen(PORT, () => {
-    console.log(`blackjack-server listening on port ${PORT}`)
-})
+  console.log(`blackjack-server listening on port ${PORT}`);
+});
