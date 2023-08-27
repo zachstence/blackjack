313e29dc2d6d1b09700a211cbefab912bcb098ac
Zachary Stence <zachstence@gmail.com>
2023-08-27 15:11:05 -0500

    chore: remove unnecessary cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,11 @@
 import express from 'express';
 import http from 'http';
-import cors from 'cors';
 
 import { SocketServer as SocketServer } from './socket-server';
 
 const PORT = 3000;
 
 const app = express();
-app.use(cors());
 const httpServer = http.createServer(app);
 
 new SocketServer(httpServer)
