58d1367a798e4d7f4ad9ae1d4f606ecc63b53ea1
Zachary Stence <zachstence@gmail.com>
2023-09-01 16:18:43 -0500

    feat: favicon and meta tags

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -2,4 +2,12 @@
   import '../app.css';
 </script>
 
+<svelte:head>
+  <title>Blackjack</title>
+  <meta name="keywords" content="blackjack, multiplayer, online, realtime, game, casino" />
+  <meta name="description" content="Free online blackjack with no ads or interruptions" />
+  <meta property="og:title" content="Blackjack" />
+  <meta property="og:description" content="Free online blackjack with no ads or interruptions" />
+</svelte:head>
+
 <slot />
