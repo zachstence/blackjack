92de6127a87adb402aa28bdd43f9f1193d8bdbf8
Zachary Stence <zachstence@gmail.com>
2023-10-21 13:27:43 -0500

    chore: move deck creation/shuffling to shoe-state.ts

--- server/src/game-state/shoe-state.ts
+++ server/src/game-state/shoe-state.ts
@@ -1,29 +1,26 @@
-import { IShoe } from "blackjack-types";
+import { IShoe, Rank, Suit } from "blackjack-types";
 import { ToClientJSON } from "./to-client-json"
 import { CardState } from "./card-state";
 
-import { createDeck } from "../createDeck";
-import { durstenfeldShuffle } from "../durstenfeldShuffle";
-
 export class ShoeState implements ToClientJSON<IShoe> {
     private _cards: CardState[] = []
-
+    
     private readonly numDecks = 6 // TODO control by options in the client
-
+    
     constructor() {
         this.reset()
     }
-
+    
     get cards(): CardState[] {
         return this._cards
     }
-
+    
     reset = (): void => {        
         const decks = Array.from({ length: this.numDecks }).flatMap(createDeck)
         durstenfeldShuffle(decks)
         this._cards = decks
     }
-
+    
     draw = (): CardState => {
         const card = this._cards.pop()
         if (!card) {
@@ -33,10 +30,30 @@ export class ShoeState implements ToClientJSON<IShoe> {
         }
         return card
     }
-
+    
     toClientJSON(): IShoe {
         return {
             length: this._cards.length,
         }
+    }
+}
+
+const createDeck = (): CardState[] => {
+    const cards: CardState[] = []
+    
+    for (const suit of Object.values(Suit)) {
+        for (const rank of Object.values(Rank)) {
+            cards.push(new CardState(suit, rank))
+        }
+    }
+    
+    return cards
+}  
+
+/** https://stackoverflow.com/a/12646864 */
+const durstenfeldShuffle = (array: unknown[]) => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
     }
 }
