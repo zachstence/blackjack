bf58a10220f3e1999c8fd3862dca2d5a05e95527
Zachary Stence <zachstence@gmail.com>
2023-10-26 15:06:37 -0500

    feat: customize materials in obj

--- frontend/src/lib/3d/Chip/ChipOBJ.svelte
+++ frontend/src/lib/3d/Chip/ChipOBJ.svelte
@@ -1,21 +1,38 @@
 <script lang="ts">
-  import { T, useLoader } from '@threlte/core';
+  import { T } from '@threlte/core';
+  import { Mesh, MeshStandardMaterial } from 'three';
   import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
+  import { getMaterialsArray } from './getMaterialsArray';
 
-  // const mtlLoader = new MTLLoader();
-  // mtlLoader.load('static/3d/Poker Chip v2.mtl', materials => {
-  //   materials.preload()
+  const base = new MeshStandardMaterial({
+    color: '#3da5ff',
+  });
+  const stripes = new MeshStandardMaterial({
+    color: '#d5dfe8',
+  });
 
-  //   const objLoader = new OBJLoader()
-  //   objLoader.setMaterials(materials)
-  //   objLoader.load('static/3d/Poker Chip v2.obj', object => {
+  const promise = new Promise((resolve) => {
+    const objLoader = new OBJLoader();
 
-  //   })
-  // });
+    console.log({ objLoader });
 
-  const object = useLoader(OBJLoader).load('static/3d/Poker Chip v2.obj');
+    objLoader.load('static/3d/Poker Chip v9.obj', async (object) => {
+      const materials = await getMaterialsArray('static/3d/Poker Chip v9.obj', {
+        Red_Plastic: base,
+        White_Plastic: stripes,
+      });
+
+      object.traverse((child) => {
+        if (child instanceof Mesh) {
+          child.material = materials;
+        }
+      });
+
+      resolve(object);
+    });
+  });
 </script>
 
-{#if $object}
-  <T is={$object} />
-{/if}
+{#await promise then object}
+  <T is={object} />
+{/await}
