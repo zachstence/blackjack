ddca952fea38de63b498e7f2a3bc417208144b7f
Zachary Stence <zachstence@gmail.com>
2023-11-01 12:03:29 -0500

    feat: environment

--- frontend/src/lib/3d/TestScene.svelte
+++ frontend/src/lib/3d/TestScene.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
   import { T } from '@threlte/core';
   import { OrbitControls } from '@threlte/extras';
+  import { Environment } from '@threlte/extras';
 </script>
+
+<Environment isBackground files="/static/3d/casino.jpg" />
 
 <T.PerspectiveCamera makeDefault position={[80, 80, 80]}>
   <OrbitControls />
 </T.PerspectiveCamera>
 
-<T.DirectionalLight position={[30, 80, 80]} />
-<T.DirectionalLight position={[-30, 80, -80]} intensity={0.2} />
 <T.AmbientLight intensity={0.2} />
 
 <slot />
