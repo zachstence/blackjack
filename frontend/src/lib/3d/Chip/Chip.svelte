edf498c643486ef2fb6e2ca232808fd228fdf7da
Zachary Stence <zachstence@gmail.com>
2023-11-03 11:00:01 -0500

    fix: use DirectionalLight instead of PointLight

--- frontend/src/lib/3d/Chip/Chip.svelte
+++ frontend/src/lib/3d/Chip/Chip.svelte
@@ -12,9 +12,7 @@
   const filletSegments = 8;
 </script>
 
-<T.AxesHelper args={[diameter]} />
-
 <T.Mesh>
   <T is={ChipGeometry(radius, height, edgeRadius, radialSegments, heightSegments, filletSegments)} />
-  <T.MeshStandardMaterial color="red" />
+  <T.MeshStandardMaterial color="white" />
 </T.Mesh>
--- frontend/src/lib/3d/TestScene.svelte
+++ frontend/src/lib/3d/TestScene.svelte
@@ -4,12 +4,16 @@
   import { Environment } from '@threlte/extras';
 </script>
 
-<Environment isBackground files="/static/3d/casino.jpg" />
+<!-- <Environment isBackground files="/static/3d/casino.jpg" /> -->
+
+<T.AxesHelper args={[100]} />
 
 <T.PerspectiveCamera makeDefault position={[80, 80, 80]}>
   <OrbitControls />
 </T.PerspectiveCamera>
 
-<T.AmbientLight intensity={0.2} />
+<T.DirectionalLight position={[100, 100, 50]} intensity={0.5} />
+<T.DirectionalLight position={[-100, 50, -100]} intensity={0.2} />
+<T.AmbientLight intensity={0.5} />
 
 <slot />
