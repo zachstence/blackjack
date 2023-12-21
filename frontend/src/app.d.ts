576c5901f9d149177d14d91385c430e10b222e56
Zachary Stence <zachstence@gmail.com>
2023-12-21 12:06:13 -0600

    Revert "feat: working websocket integration in SvelteKit"
    This reverts commit dd2f416ae1adefb141486f4ce3e8f5a6a6d30aa2.

--- frontend/src/app.d.ts
+++ frontend/src/app.d.ts
@@ -4,8 +4,7 @@ declare global {
   namespace App {
     // interface Error {}
     interface Locals {
-      auth: import('lucia').AuthRequest;
-      wss: import('$lib/server/ws').ExtendedWebSocketServer;
+      auth: import("lucia").AuthRequest;
     }
     // interface PageData {}
     // interface Platform {}
@@ -14,14 +13,14 @@ declare global {
 
 /// <reference types="lucia" />
 declare global {
-  namespace Lucia {
-    type Auth = import('$lib/server/lucia').Auth;
-    type DatabaseUserAttributes = {
-      username: string;
+	namespace Lucia {
+		type Auth = import("$lib/server/lucia").Auth;
+		type DatabaseUserAttributes = {
+      username: string
     };
-    // eslint-disable-next-line @typescript-eslint/ban-types
-    type DatabaseSessionAttributes = {};
-  }
+		// eslint-disable-next-line @typescript-eslint/ban-types
+		type DatabaseSessionAttributes = {};
+	}
 }
 
 export {};
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,23 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-import { createWSSGlobalInstance, onHttpServerUpgrade } from './src/lib/server/ws';
-
 export default defineConfig({
-  plugins: [
-    sveltekit(),
-    {
-      name: 'integratedWebsocketServer',
-      configureServer: (server) => {
-        createWSSGlobalInstance();
-        server.httpServer?.on('upgrade', onHttpServerUpgrade);
-      },
-      configurePreviewServer: (server) => {
-        createWSSGlobalInstance();
-        server.httpServer?.on('upgrade', onHttpServerUpgrade);
-      },
-    },
-  ],
+  plugins: [sveltekit()],
   ssr: {
     noExternal: ['three'],
   },
