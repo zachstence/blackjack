b6cfe5c26d97c50e8153d35ef0197f0759848105
Zachary Stence <zachstence@gmail.com>
2023-12-30 13:39:38 -0600

    fix: dont instantiate two prisma clients

--- frontend/src/lib/server/lucia.ts
+++ frontend/src/lib/server/lucia.ts
@@ -1,16 +1,15 @@
 import { lucia } from 'lucia';
 import { sveltekit } from 'lucia/middleware';
 import { prisma } from '@lucia-auth/adapter-prisma';
-import { PrismaClient } from '@prisma/client';
 import { dev } from '$app/environment';
 
-const client = new PrismaClient();
+import { prisma as prismaClient } from './prisma';
 
 // expect error (see next section)
 export const auth = lucia({
   env: dev ? 'DEV' : 'PROD',
   middleware: sveltekit(),
-  adapter: prisma(client),
+  adapter: prisma(prismaClient),
 
   getUserAttributes: (data) => data,
 });
