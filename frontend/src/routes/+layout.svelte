03aee30f12f874e583a4f474e8f14395986b6514
Zachary Stence <zachstence@gmail.com>
2023-12-29 20:37:37 -0600

    chore: remove redundant (app) route group

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,5 +1,12 @@
-<script>
+<script lang="ts">
   import '../app.css';
+  import { page } from '$app/stores';
+  import type { PageData } from './$types';
+  import Header from './_components/Header.svelte';
+
+  export let data: PageData;
+
+  $: showHeader = $page.route.id !== '/play/[tableId]';
 </script>
 
 <svelte:head>
@@ -9,5 +16,9 @@
   <meta property="og:title" content="Blackjack" />
   <meta property="og:description" content="Free online blackjack with no ads or interruptions" />
 </svelte:head>
+
+{#if showHeader}
+  <Header user={data.user} />
+{/if}
 
 <slot />
