1c8621af5b3d9857a8af58340c6e073217c97503
Zachary Stence <zachstence@gmail.com>
2023-12-29 20:30:12 -0600

    feat: show players in game

--- frontend/src/routes/(app)/play/[tableId]/_components/Game/Game.svelte
+++ frontend/src/routes/(app)/play/[tableId]/_components/Game/Game.svelte
@@ -1,12 +1,19 @@
 <script lang="ts">
   import { getTableStoreContext } from '../../TableStore';
+  import Player from './Player.svelte';
 
   let clazz = '';
   export { clazz as class };
 
   const tableStore = getTableStoreContext();
+
+  $: players = Object.values($tableStore.players);
 </script>
 
-<div class={clazz}>Game</div>
-
-<pre class="text-sm">{JSON.stringify($tableStore, null, 2)}</pre>
+<div class="{clazz} flex flex-row gap-4 p-4">
+  <div class="2-fit flex flex-col gap-2 border border-black p-2">
+    {#each players as player}
+      <Player {player} />
+    {/each}
+  </div>
+</div>
---
+++ frontend/src/routes/(app)/play/[tableId]/_components/Game/Player.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import type { Player } from '$lib/types/realtime';
+
+  export let player: Player;
+</script>
+
+<div class="flex flex-col gap-2 px-4 py-2 bg-base-200 rounded-xl">
+  <span class="font-semibold">{player.name}</span>
+</div>
