47516f2759b1d103afaccb494ceaa61d0592c89d
Zachary Stence <zachstence@gmail.com>
2023-12-30 17:27:16 -0600

    refactor: better abstraction for SSE

--- frontend/src/lib/server/services/sse-service.ts
+++ frontend/src/lib/server/services/sse-service.ts
@@ -1,28 +1,49 @@
+import type { ServerEvent } from '$lib/types/realtime';
+import { nanoid } from 'nanoid';
+
+export type AddClientArgs = {
+  onConnect: (clientId: string) => void | Promise<void>;
+  onDisconnect: (clientId: string) => void | Promise<void>;
+};
+
+export type AddClientResult = {
+  clientId: string;
+  response: Response;
+};
+
 export class SSEService {
   private clients: { [clientId: string]: ReadableStreamDefaultController } = {};
 
-  addClient = (id: string, controller: ReadableStreamDefaultController): void => {
-    console.log(`[sse] Adding client with ID ${id}`);
-    if (id in this.clients) {
-      throw new Error(`[sse] Client with ID ${id} is already connected`);
+  addClient = (args: AddClientArgs): AddClientResult => {
+    const clientId = nanoid();
+    console.log(`[sse] Adding client with ID ${clientId}`);
+
+    if (clientId in this.clients) {
+      throw new Error(`[sse] Client with ID ${clientId} is already connected`);
     }
-    this.clients[id] = controller;
+
+    const body = new ReadableStream({
+      start: async (controller) => {
+        this.clients[clientId] = controller;
+        await args.onConnect(clientId);
+      },
+      cancel: async () => {
+        delete this.clients[clientId];
+        await args.onDisconnect(clientId);
+      },
+    });
+
+    const headers = {
+      'Content-Type': 'text/event-stream',
+      Connection: 'keep-alive',
+      'Cache-Control': 'no-cache',
+    };
+
+    const response = new Response(body, { headers });
+    return { clientId, response };
   };
 
-  removeClient = (id: string): void => {
-    console.log(`[sse] Removing client with ID ${id}`);
-    delete this.clients[id];
-  };
-
-  getClient = (id: string): ReadableStreamDefaultController | undefined => {
-    return this.clients[id];
-  };
-
-  getClients = (): readonly [string, ReadableStreamDefaultController][] => {
-    return Object.entries(this.clients);
-  };
-
-  send = (clientId: string, data: Record<string, unknown>): void => {
+  send = (clientId: string, data: ServerEvent): void => {
     const controller = this.clients[clientId];
     if (!controller) {
       console.debug(`Client ${clientId} not connected, can't send event`);
--- frontend/src/routes/play/[tableId]/+server.ts
+++ frontend/src/routes/play/[tableId]/+server.ts
@@ -1,5 +1,4 @@
 import { error } from '@sveltejs/kit';
-import { nanoid } from 'nanoid';
 import type { RequestHandler } from './$types';
 
 import { sseService, tableService } from '$lib/server';
@@ -9,54 +8,42 @@ export const GET: RequestHandler = async ({ params, locals }) => {
   const session = await locals.auth.validate();
   if (!session) throw error(400);
 
-  const player: Player = {
-    id: session.user.userId,
-    sseClientId: nanoid(),
-    tableId: params.tableId,
-    name: session.user.username,
+  const { user } = session;
+  const { tableId } = params;
+
+  const onConnect = async (clientId: string): Promise<void> => {
+    const player: Player = {
+      sseClientId: clientId,
+      tableId: tableId,
+      id: user.userId,
+      name: user.username,
+    };
+
+    const table = await tableService.addPlayer(tableId, player);
+    Object.values(table.players).forEach((p) => {
+      sseService.send(p.sseClientId, {
+        path: 'players',
+        value: table.players,
+      });
+    });
   };
 
-  return createEventStreamResponse(params.tableId, player);
-};
-
-const createEventStreamResponse = (tableId: string, player: Player): Response => {
-  const body = new ReadableStream({
-    start: async (controller) => {
-      sseService.addClient(player.sseClientId, controller);
-
-      const table = await tableService.addPlayer(tableId, player);
-
+  const onDisconnect = async (): Promise<void> => {
+    const table = await tableService.removePlayer(tableId, user.userId);
+    if (Object.values(table.players).length === 0) {
+      // Delete table if no more players
+      await tableService.remove(tableId);
+    } else {
+      // Otherwise update table state
       Object.values(table.players).forEach((p) => {
         sseService.send(p.sseClientId, {
           path: 'players',
           value: table.players,
         });
       });
-    },
-    cancel: async () => {
-      sseService.removeClient(player.sseClientId);
-      const table = await tableService.removePlayer(tableId, player.id);
-
-      if (Object.values(table.players).length === 0) {
-        // Delete table if no more players
-        await tableService.remove(tableId);
-      } else {
-        // Otherwise update table state
-        Object.values(table.players).forEach((p) => {
-          sseService.send(p.sseClientId, {
-            path: 'players',
-            value: table.players,
-          });
-        });
-      }
-    },
-  });
-
-  const headers = {
-    'Content-Type': 'text/event-stream',
-    Connection: 'keep-alive',
-    'Cache-Control': 'no-cache',
+    }
   };
 
-  return new Response(body, { headers });
+  const { response } = sseService.addClient({ onConnect, onDisconnect });
+  return response;
 };
